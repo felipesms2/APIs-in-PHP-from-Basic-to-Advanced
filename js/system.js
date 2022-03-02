578518d343d0b8ad292194dd94ff9e4f6db0562d
felipesms2 <felipesms@live.com>
2022-03-02 05:15:59 -0300

    module templates

---
+++ js/system.js
@@ -0,0 +1,13 @@
+
+            module = [];
+            module[1] = 
+                {
+                    idModule: 1, 
+                    nameModule: "Cadastro de Questões", 
+                    viewName: "question-manage-body",
+                };
+            module[2] = {idModule: 2, nameModule: "Relatórios", viewName: "reports-main"};
+            module[3] = {idModule: 2, nameModule: "Suporte", viewName: "support"};
+            module[3] = {idModule: 3, nameModule: "Questionario", viewName: "run-questions"};
+
+            
