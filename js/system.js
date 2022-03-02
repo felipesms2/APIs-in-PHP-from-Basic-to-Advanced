72b59446483d02c8dfe1c7125109da93d631a700
felipesms2 <felipesms@live.com>
2022-03-02 16:01:36 -0300

    Method for load spa pages

--- js/system.js
+++ js/system.js
@@ -1,11 +1,30 @@
 
             module = [];
+            module[1] = 
+                {
+                    idModule: 2, 
+                    nameModule: "Initial Page", 
+                    viewName: "start-page"
+                };      
             module[2] = 
                 {
                     idModule: 2, 
                     nameModule: "2. Make an API call access an API from PHP.mp4", 
                     viewName: "2.2"
-                };
-            
-            
+                };      
 
+            function loadModule(idModule) 
+                {
+        
+                    //console.log(JSON.stringify(module[1]));
+                    titleElement = "module-title";
+                    titleValue = module[idModule].nameModule;
+                    viewName = module[idModule].viewName;
+                    $("#" +  titleElement).html(titleValue); 
+                    document.title = titleValue;
+                    loadView(viewName);     
+                }
+            function loadView(viewName, fileExtension="php") 
+                {
+                    $("#main-content").load("Views/" + viewName + "." + fileExtension , function(){/*do something assync*/});
+                }
